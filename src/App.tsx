4709438395fe02dc60566630389044e193e8b638
Gabriel Fernando <gf313074@gmail.com>
2025-04-19 00:09:53 -0300

    create form and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,48 @@
-function App() {
+import { Button } from './components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from './components/ui/card';
+import { Input } from './components/ui/input';
+import { Label } from './components/ui/label';
+import { Separator } from './components/ui/separator';
 
+function App() {
   return (
-    <>
-     <h1>Cade</h1>
-    </>
-  )
+    <main className='h-screen flex w-full'>
+      <div className='bg-primary-foreground w-full h-full flex p-16'></div>
+      <section className='flex items-center justify-center bg-background h-full max-w-3xl w-full p-4'>
+        <Card className='w-96'>
+          <CardHeader>
+            <CardTitle
+              children='Entre com sua conta'
+              className='text-2xl font-bold tracking-tighter'
+            />
+            <CardDescription children='Utilize seu e-mail e senha ou o Github para entrar.' />
+          </CardHeader>
+          <CardContent>
+            <div>
+              <Label htmlFor='email' children='E-mail' />
+              <Input type='email' id='email' placeholder='exemplo@email.com' />
+            </div>
+            <div className='mt-16'>
+              <Label htmlFor='password' children='Senha' />
+              <Input type='password' placeholder='Senha' id='password' />
+            </div>
+            <Button children='Entrar' className='mt-6 w-full' />
+            <div className='flex items-center gap-6'>
+              <Separator>
+                <span className='text-xs text-muted-foreground'>OU</span>
+              </Separator>
+            </div>
+          </CardContent>
+        </Card>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
